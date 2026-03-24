57942eee6f626b84385b95199e3f67838e03afcf
aymen mejri <noise_team@hotmail.com>
2026-03-23 22:30:10 -0400

    Add transaction component

--- src/Components/Router.tsx
+++ src/Components/Router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Category from './Category.tsx';
 import { Transaction } from './Transaction.tsx';
+import { AddTransaction } from './AddTransaction.tsx';
 
 export default function Router() {
     return (
@@ -8,6 +9,7 @@ export default function Router() {
             <Routes>
                 <Route path="/" element={<Category />} />
                 <Route path="/transactions" element={<Transaction />} />
+                <Route path="/add-transaction" element={<AddTransaction />} />
             </Routes>
         </BrowserRouter>
     );
